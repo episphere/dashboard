1f43f8cbbba1cd847077cbde7055f5661d218e52
Jonas Almeida <jalmeida@mathbiol.org>
2019-05-09 12:48:07 -0400

    michelle forced me to call it recruitment

--- enrollConnect_1/enrollConnect.js
+++ enrollConnect_1/enrollConnect.js
@@ -11,7 +11,7 @@ enroll.ui=function(div){ // build user interface if
         enroll.div.innerHTML=`
         <a href="https://dceg.cancer.gov/research/who-we-study/cohorts/connect" target="_blank">
         <image height="40px" src="../connect.png"></a>
-        <b style="font-size:xx-large;color:maroon;white-space:nowrap">Enrollment Dashboard</b>
+        <b style="font-size:xx-large;color:maroon;white-space:nowrap">Recruitment Dashboard</b>
         <div id="enrollDash">
         <p>Site Key: <input size=30 type="password" id="key" value="${enroll.store.key}"><button id="connectKey" onclick="enroll.withKey()">Connect</button><input type="checkbox" id="checkSave"><span style="font-size:small;color:black">save</span></p>
         </div>
