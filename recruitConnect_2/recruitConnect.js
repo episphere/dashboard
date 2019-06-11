0b192b6274c7f288788d6a11cf697456baf4c8ed
bhaumik55231 <bhaumik55231@gmail.com>
2019-06-11 12:17:22 -0400

    Changing logos depending on API key

--- recruitConnect_2/recruitConnect.js
+++ recruitConnect_2/recruitConnect.js
@@ -1,4 +1,5 @@
 import { table } from './components/table.js'
+import { template } from './components/siteSpecificLogos.js';
 
 let recruit={}
 recruit.gitIcon=document.createElement('span').innerHTML='<svg class="octicon octicon-mark-github v-align-middle" height="32" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg>'
@@ -50,7 +51,33 @@ recruit.dashLogin=async function(){ // build the dashboard after logged with key
             headers:{
                 "Authorization":`Bearer ${recruit.key}`
             }
-        })).json()
+        })).json();
+        let siteLogoPath = '';
+        if(recruit.key === 'FHYFHORxImxG6nlbfpfj'){
+            siteLogoPath = './images/Chicago_Logo.png';
+        }
+        else if(recruit.key === 'AaJSCDZqGLqIxYxsjiob'){
+            siteLogoPath = './images/HP_Logo_png.png';
+        }
+        else if(recruit.key === 'lSP3Sz6FnyfV7imOlvjF'){
+            siteLogoPath = './images/Henry_Ford_Logo.png';
+        }
+        else if(recruit.key === 'Rd69wieaphftOeTRiQJQ'){
+            siteLogoPath = './images/Kaiser_Logo.png';
+        }
+        else if(recruit.key === 'dZD798U6AJU7cphyKc7H'){
+            siteLogoPath = './images/Marshfield_Logo.png';
+        }
+        else if(recruit.key === 'oXAG9rlxKEUChnarQqtj'){
+            siteLogoPath = './images/Norc_Logo.png';
+        }
+        else if(recruit.key === 'ac6i1hk9tMH4oNZO3KJM'){
+            siteLogoPath = './images/Sanford_Logo.png';
+        }
+        else{
+            siteLogoPath = './images/nci_logo.jpg';
+        }
+        document.getElementById('siteSpecificLogos').innerHTML = template(siteLogoPath)
         recruit.dashUI(files)
     } catch(e){
         recruit.dash.innerHTML='<p style="color:red">login failed - invalid key?</p>'
