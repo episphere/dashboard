0b9a8840e2cc65e82e9e97a0563312780a5c3559
bhaumik55231 <bhaumik55231@gmail.com>
2019-06-11 14:24:23 -0400

    Bug Fix

--- recruitConnect_2/components/siteSpecificLogos.js
+++ recruitConnect_2/components/siteSpecificLogos.js
@@ -1,7 +1,7 @@
 export function template(site_logo) {
     return `
         <div class="row">
-            <div class="col"><img class="site-specific-logo" src="../../connect.png" alt=""></div>
+            <div class="col"><img class="site-specific-logo" src="../connect.png" alt=""></div>
             <div class="col align-right"><img class="site-specific-logo" src="${site_logo}" alt=""></div>
         </div>
     `;
