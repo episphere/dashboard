e95cd1c1523d195b94db0a98d4c34d1d801c67c9
Jonas Almeida <jalmeida@mathbiol.org>
2019-08-09 13:02:09 -0400

    passing parameters to page and hide them there

--- recruitParticipant/recruitParticipant.js
+++ recruitParticipant/recruitParticipant.js
@@ -17,19 +17,44 @@ recruit.ui=function(div){ // build user interface if
         <a href="https://github.com/episphere/dashboard/tree/master/recruitParticipant" target="_blank">
         <svg id="githubIcon" style="fill:navy;height:22" class="octicon octicon-mark-github v-align-middle" height="32" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg></a>
         <a href="https://documenter.getpostman.com/view/7490604/SVYnT26j" target="_blank"><img height=22 src="https://assets.getpostman.com/common-share/postman-logo-horizontal-white.svg"></a>
-        <p style="color:gray">Use something with the form <a href="https://episphere.github.io/dashboard/recruitParticipant/?site={siteNumId}&token={participantToken}" target="_blank">https://episphere.github.io/dashboard/recruitParticipant/?site={siteNumId}&token={participantToken}</a> for an example of a tokenized trajectory.</p>
+        <p style="color:gray">Use something with the form <a href="https://episphere.github.io/dashboard/recruitParticipant/#site=123&token=abc" target="_blank">https://episphere.github.io/dashboard/recruitParticipant/#site={siteId}&token={participantToken}</a> for an example of a tokenized trajectory. The code accomodates search and hash (try the same link with # instead of ?), or a combination of the two. The preference for the hash is that it can be wiped from the URL and from prying eyes. If the page is then reloaded, or returned to later, the latest version of teh parameters, whicever they may be, is retrieved. Give it a try.</p>
         <div id="recruitDash">
         ...
         </div>
         `
         // check parameters in location.search
-        if(location.search.length){
+        if(location.search.length>2){
             location.search.slice(1).split('&').forEach(pp=>{
                 pp = pp.split('=')
                 recruit.parms[pp[0]]=pp[1]
                 //debugger
             })
-        } 
+        }
+        // check parameters in location.hash <-- note hash happens after search to we get precedence in parameter defenitions passed by hash
+        if(location.hash.length>2){
+            location.hash.slice(1).split('&').forEach(pp=>{
+                pp = pp.split('=')
+                recruit.parms[pp[0]]=pp[1]
+                //debugger
+            })
+        }
+        // update localStorage
+        let parms={}
+        if(localStorage.recruitParms){parms=JSON.parse(localStorage.recruitParms)}
+        Object.keys(recruit.parms).forEach(p=>{
+            parms[p]=recruit.parms[p]
+        })
+        localStorage.recruitParms=JSON.stringify(parms)
+        recruit.parms=parms
+
+        // clean hash
+        location.hash=''
+        
+        // report parameters
+        const liParms=document.createElement('li')
+        liParms.innerHTML=`Parameters: <pre>${JSON.stringify(recruit.parms,null,3)}</pre>`
+        recruitDash.appendChild(liParms)
+        //debugger
         
 
 
