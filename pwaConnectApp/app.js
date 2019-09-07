4994285fef31d6bd275c91bd74669dd63fa69853
bhaumik55231 <bhaumik55231@gmail.com>
2019-09-07 15:26:49 -0400

    small updates to email auth

--- pwaConnectApp/app.js
+++ pwaConnectApp/app.js
@@ -288,7 +288,7 @@ const signIn = () => {
     };
     !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
     const auth = new firebase.auth();
-    const ui =new firebaseui.auth.AuthUI(firebase.auth());
+    const ui = new firebaseui.auth.AuthUI(firebase.auth());
     const mainContent = document.getElementById('root');
     mainContent.innerHTML = '';
     ui.start('#root', {
@@ -296,7 +296,10 @@ const signIn = () => {
         signInOptions: [
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
             firebase.auth.GithubAuthProvider.PROVIDER_ID,
-            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            {
+                provider:firebase.auth.EmailAuthProvider.PROVIDER_ID,
+                signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+            },
             firebase.auth.PhoneAuthProvider.PROVIDER_ID
         ]
     });
