edadd2d60d62347f30f5b423a45afbd4eb53ee58
bhaumik55231 <bhaumik55231@gmail.com>
2019-09-07 13:34:27 -0400

    removing twitter and facebook authentication

--- pwaConnectApp/app.js
+++ pwaConnectApp/app.js
@@ -294,8 +294,6 @@ const signIn = () => {
         signInSuccessUrl: '#account_created',
         signInOptions: [
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
             firebase.auth.GithubAuthProvider.PROVIDER_ID,
             firebase.auth.EmailAuthProvider.PROVIDER_ID,
             firebase.auth.PhoneAuthProvider.PROVIDER_ID
