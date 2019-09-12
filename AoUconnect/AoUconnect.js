883b6e3f84394632c4ac9308f6cbcaa256ed2b09
Jonas Almeida <jalmeida@mathbiol.org>
2019-09-12 10:12:05 -0400

    AoU mock UI

--- AoUconnect/AoUconnect.js
+++ AoUconnect/AoUconnect.js
@@ -1,1 +1,17 @@
-console.log('mrn.js started')
+console.log('AoUconnect.js started')
+
+AoUconnect=function(){ // ini
+    // check env and initialize UI is fitting
+    // ...
+}
+
+AoUconnect.check=function(el){
+    if(el.checked){
+        connectEnrolled.innerText='enrolled, thank you!'
+        connectEnrolled.style.color='green'
+    }else{
+        connectEnrolled.innerText='not enrolled'
+        connectEnrolled.style.color='orange'
+    }
+    //debugger
+}
