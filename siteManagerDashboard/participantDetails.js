8803fae6904a752297e763e8cbab8adfc9dd9226
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2021-01-04 11:49:59 -0500

    added few k-v pairs & fixed few bugs

--- siteManagerDashboard/participantDetails.js
+++ siteManagerDashboard/participantDetails.js
@@ -1,10 +1,11 @@
 import {renderNavBarLinks, dashboardNavBarLinks, renderLogin, removeActiveClass} from './navigationBar.js';
 import fieldMapping from './fieldToConceptIdMapping.js'; 
-export const headerImportantColumns = ['Connect_ID', fieldMapping.fName, fieldMapping.birthYear, 
-    fieldMapping.consentDate ]
-export const importantColumns = [fieldMapping.lName, fieldMapping.fName, fieldMapping.prefName, fieldMapping.mName, fieldMapping.birthMonth, fieldMapping.birthDay, fieldMapping.birthYear, 
-    fieldMapping.homePhone, fieldMapping.cellPhone, fieldMapping.otherPhone, fieldMapping.prefEmail, fieldMapping.prefContact, fieldMapping.canWeText,
-    fieldMapping.address1, fieldMapping.address2, fieldMapping.city, fieldMapping.state, fieldMapping.zip, 'Connect_ID'];
+
+export const headerImportantColumns = ['Connect_ID', fieldMapping.fName, fieldMapping.birthYear, fieldMapping.consentDate]
+
+export const importantColumns = [fieldMapping.lName, fieldMapping.fName, fieldMapping.prefName, fieldMapping.mName, fieldMapping.suffix, fieldMapping.birthMonth, fieldMapping.birthDay, fieldMapping.birthYear, 
+    fieldMapping.homePhone, fieldMapping.cellPhone, fieldMapping.otherPhone, fieldMapping.prefEmail, fieldMapping.prefContact, fieldMapping.canWeText, fieldMapping.ssnOnFile,
+    fieldMapping.address1, fieldMapping.city, fieldMapping.state, fieldMapping.zip, 'Connect_ID'];
 
 
 export function renderParticipantDetails(participant){
@@ -28,6 +29,7 @@ export function render(participant) {
          `
     } else {
         let conceptIdMapping = JSON.parse(localStorage.getItem('conceptIdMapping'));
+        console.log('conceptIdMapping', conceptIdMapping)
         template += `
             <div class="container">
                 <div id="root">
@@ -38,8 +40,8 @@ export function render(participant) {
         template += `</div><form><table class="table"> <h4 style="text-align: center;"> Participant Details </h4><tbody>`
        
         importantColumns.forEach(x => template += `<tr><th scope="row"><div class="mb-3"><label class="form-label">
-            ${conceptIdMapping[x] && conceptIdMapping[x] ? conceptIdMapping[x]['Variable Label'] || conceptIdMapping[x]['Variable Name'] : x}</label></div></th> :
-            <td>${participant[x]}</td> <td><button type="button" class="btn btn-primary">Edit</button></td></tr>&nbsp;`)
+            ${conceptIdMapping[x] && conceptIdMapping[x] ? conceptIdMapping[x]['Variable Label'] || conceptIdMapping[x]['Variable Name'] : x}</label></div></th>
+            <td>${participant[x] !== undefined ?  participant[x] : ""}</td> <td><button type="button" class="btn btn-primary">Edit</button></td></tr>&nbsp;`)
 
         template += `</tbody></table>
                     <table class="table">
