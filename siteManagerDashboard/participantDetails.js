8b13caa9e0ad3f1ff0323f593527d3d7b4e4e0eb
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2021-01-05 20:03:32 -0500

    added a modal to show data needs some fixes

--- siteManagerDashboard/participantDetails.js
+++ siteManagerDashboard/participantDetails.js
@@ -1,7 +1,7 @@
 import {renderNavBarLinks, dashboardNavBarLinks, renderLogin, removeActiveClass} from './navigationBar.js';
 import fieldMapping from './fieldToConceptIdMapping.js'; 
 
-export const headerImportantColumns = ['Connect_ID', fieldMapping.fName, fieldMapping.birthYear, fieldMapping.consentDate]
+export const headerImportantColumns = ['Connect_ID', fieldMapping.fName, fieldMapping.birthYear, fieldMapping.consentDate, 'Years in Connect']
 
 export const importantColumns = [fieldMapping.lName, fieldMapping.fName, fieldMapping.prefName, fieldMapping.mName, fieldMapping.suffix, fieldMapping.birthMonth, fieldMapping.birthDay, fieldMapping.birthYear, 
     fieldMapping.homePhone, fieldMapping.cellPhone, fieldMapping.otherPhone, fieldMapping.prefEmail, fieldMapping.prefContact, fieldMapping.canWeText, fieldMapping.ssnOnFile,
@@ -14,7 +14,7 @@ export function renderParticipantDetails(participant){
     removeActiveClass('nav-link', 'active');
     document.getElementById('participantDetailsBtn').classList.add('active');
     mainContent.innerHTML = render(participant);
-    changeParticipantDetail()
+    changeParticipantDetail(participant) 
 
 
 }
@@ -37,16 +37,28 @@ export function render(participant) {
             <div class="container">
                 <div id="root">
                     <div class="alert alert-light" role="alert">`
-        headerImportantColumns.forEach(x => template += `<span><b>${conceptIdMapping[x] && conceptIdMapping[x] ? conceptIdMapping[x]['Variable Label']
-        || conceptIdMapping[x]['Variable Name'] : x}</b></span> : ${participant[x]} &nbsp;`)
+        headerImportantColumns.forEach(x => template += `<span><b>
+                ${conceptIdMapping[x] && conceptIdMapping[x] != 'Years in Connect' ? 
+                conceptIdMapping[x]['Variable Label'] || conceptIdMapping[x]['Variable Name'] 
+                : 
+                getYearsInConnect()}
+                </b></span> : ${participant[x] !== undefined ?  participant[x] : ""} &nbsp;`)
         
         template += `</div><form><table class="table"> <h4 style="text-align: center;"> Participant Details </h4><tbody>`
        
-        importantColumns.forEach(x => template += `<tr><th scope="row"><div class="mb-3"><label class="form-label">
+        importantColumns.forEach(x => template += `<tr ><th scope="row"><div class="mb-3"><label class="form-label">
             ${conceptIdMapping[x] && conceptIdMapping[x] ? conceptIdMapping[x]['Variable Label'] || conceptIdMapping[x]['Variable Name'] : x}</label></div></th>
-            <td>${participant[x] !== undefined ?  participant[x] : ""}</td> <td> <a id="showMore" ><button type="button" class="btn btn-primary">Edit</button></a></td></tr>&nbsp;`)
-          
-        template += `</tbody></table>
+            <td>${participant[x] !== undefined ?  participant[x] : ""}</td> <td> <a  data-toggle="modal" data-target="#modalShowMoreData" data-participantInfo="oolol" name="modalParticipantData" id="showMore"  ><button type="button" class="btn btn-primary">Edit</button></a></td></tr>&nbsp;`) 
+        
+            template += ` <div class="modal fade" id="modalShowMoreData" data-keyboard="false" data-backdrop="static" tabindex="-1" role="dialog" data-backdrop="static" aria-hidden="true">
+                            <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                                <div class="modal-content sub-div-shadow">
+                                    <div class="modal-header" id="modalHeader"></div>
+                                    <div class="modal-body" id="modalBody"></div>
+                                </div>
+                            </div>
+                        </div>`
+            template += `</tbody></table>
                     <table class="table">
                                 <h4 style="text-align: center;"> Alternate Contact Details &nbsp; <button type="button" id="showMore" class="btn btn-primary">Edit</button> </h4>
                                     <thead>
@@ -91,10 +103,34 @@ export function render(participant) {
 }
 
 export function changeParticipantDetail(){
-    console.log("123456")
+    const currentDate = new Date();
     const a = document.getElementById('showMore')
+  //  let data = getDataAttributes(a)
+    console.log('a9999999', a)
+   let text = "data.participantinfo"
+
     if (a) {
-        a.addEventListener('click', click_handler1);
+        a.addEventListener('click', () => {
+                    console.log("text", text)
+                    // let temp = document.createElement("div")
+                    // temp.innerHTML = text
+                    // console.log("conceptId", temp)
+                    const header = document.getElementById('modalHeader');
+                    const body = document.getElementById('modalBody');
+                    header.innerHTML = `<h5>Soul</h5><button type="button" class="modal-close-btn" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>`
+                    let template = '<div>'
+                    template += `
+                        <ul>
+                            <li><span><strong>Current value</span></strong> :  <span>${text}</span></li>
+                            <li><span><strong>New value</span></strong> :  <input required type="text" name="newValue" id="newValue"></li>
+                            <li><span><strong>Time stamp</span></strong> :  <span>${currentDate.getTime()}</span></li>
+                            <li><span><strong>Comment</span></strong> :  <input required type="text" name="comment" id="comment"></li>
+                            <li><span><strong>User ID</span></strong> :  <span>${currentDate.getTime()}</span></li>
+                        </ul>
+                        </div>`
+                    body.innerHTML = template;
+             
+        });
     }
     else {
         console.log("test123", a)
@@ -124,8 +160,23 @@ export function changeParticipantDetail(){
     
     // mainContent.innerHTML = template;
 }
+function getDataAttributes(el) {
+    var data = {};
 
-function click_handler1() { 
-    console.log("conceptId")
-    alert("hello")
- }
+    [].forEach.call(el.attributes, function(attr) {
+        console.log('fff', attr.value)
+        if (/^data-/.test(attr.name)) {
+            var camelCaseName = attr.name.substr(5).replace(/-(.)/g, function ($0, $1) {
+                return $1.toUpperCase();
+            });
+            data[camelCaseName] = attr.value;
+        }
+    });
+    return data;
+}
+
+function getYearsInConnect() {
+    const currentDate = new Date();
+    let yearsInConnect = `Years In Connect: ${currentDate.getTime()}`
+    return yearsInConnect;
+}
