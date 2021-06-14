bab4e3862c9360d6be4d730181307e557ee3a62d
Barra <55366@icf.com>
2021-06-14 14:04:27 -0400

    style: apply prettier to html and js files

--- geneTestPlayground/index.js
+++ geneTestPlayground/index.js
@@ -1,21 +1,17 @@
 /* 
 Target and return first element with specified id, assign value to content variable
 */
-/**/
-const headerContent = document.querySelector('#header-content')
-const alertButton = document.querySelector('#alert-button')
+
+const headerContent = document.querySelector("#header-content");
+const alertButton = document.querySelector("#alert-button");
 
 /*
 onload fires function when entire page is done loading
 */
 
+window.onload = (e) => {
+  headerContent.textContent = `Hello World!`;
+};
 
-
-window.onload = e => {
-    headerContent.textContent = `Hello World!`
-}
-
-
-/* Add click event to alertButton, on click an interaction modal pops up*/
-alertButton.addEventListener('click', e => alert(`Alert, much wow!`))
-
+/* Add click event to alertButton, on click an interaction dialog box pops up*/
+alertButton.addEventListener("click", (e) => alert(`Alert, much wow!`));
