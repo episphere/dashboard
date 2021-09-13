b29613cc5e8ad9426e002ec07fe82f0e6302039a
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2021-09-13 16:46:51 -0400

    uncomment code

--- siteManagerDashboard/participantWithdrawalForm.js
+++ siteManagerDashboard/participantWithdrawalForm.js
@@ -470,7 +470,7 @@ const sendResponses = async (finalOptions, retainOptions, requestedHolder, sourc
     const token = localStorage.getItem("token");
     sendRefusalData['token'] = token;
     const siteKey = await getAccessToken();
-  //  clickHandler(sendRefusalData, siteKey, token);
+    clickHandler(sendRefusalData, siteKey, token);
 }
 
 const setRefusalTimeStamp = (sendRefusalData, optionSelected, refusalOptionTimeStamp) =>{
