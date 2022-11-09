b78718df0618290b56c7658f8ca29184909a8a09
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2022-11-09 16:03:03 -0500

    added new condition

--- siteManagerDashboard/participantSummary.js
+++ siteManagerDashboard/participantSummary.js
@@ -90,7 +90,7 @@ export const render = (participant) => {
                                 <tr class="row-color-survey-light">
                                     ${baselineBiospecSurvey(participant)}
                                 </tr>
-                                ${participant[fieldMapping.menstrualFlag] === fieldMapping.submitted1 ?
+                                ${participant[fieldMapping.combinedBoodUrineMouthwashSurvey] === fieldMapping.submitted1 && participant[fieldMapping.menstrualFlag] === fieldMapping.submitted1 ?
                                     `<tr class="row-color-enrollment-light"> ${baselineMenstrualSurvey(participant)} </tr>`
                                 : `` }                    
                                 <tr class="row-color-survey-dark">
