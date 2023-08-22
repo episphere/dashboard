14a06da00ac99b1367286050948378a059a44565
jhflorey <jhflorey@gmail.com>
2023-08-22 11:28:54 -0400

    Fix spelling

--- siteManagerDashboard/participantSummary.js
+++ siteManagerDashboard/participantSummary.js
@@ -353,16 +353,16 @@ const renderDownload = async (participant, timeStamp, fileLocation, coordinates,
 }
 
 const createSignature = (participant, type) => {
-    const mddileNameCID = type === 'hipaarevoc' ? fieldMapping.HIPAARevocMiddleName : type === 'datadestruction' ? fieldMapping.dataDestructionMiddleName : fieldMapping.consentMiddleName;
-    const middleName =  participant[mddileNameCID] ? ` ${participant[mddileNameCID]} ` : ' ';
+    const middleNameCID = type === 'hipaarevoc' ? fieldMapping.HIPAARevocMiddleName : type === 'datadestruction' ? fieldMapping.dataDestructionMiddleName : fieldMapping.consentMiddleName;
+    const middleName =  participant[middleNameCID] ? ` ${participant[middleNameCID]} ` : ' ';
     const createParticipantSignature = participant[fieldMapping.consentFirstName] + middleName + participant[fieldMapping.consentLastName]
     return createParticipantSignature;
 }
 
 const createPrintName = (participant, type) => {
     const firstName = participant[fieldMapping.consentFirstName]
-    const mddileNameCID = type === 'hipaarevoc' ? fieldMapping.HIPAARevocMiddleName : type === 'datadestruction' ? fieldMapping.dataDestructionMiddleName : fieldMapping.consentMiddleName;
-    const middleName =  participant[mddileNameCID] ? ` ${participant[mddileNameCID]} ` : ' ';
+    const middleNameCID = type === 'hipaarevoc' ? fieldMapping.HIPAARevocMiddleName : type === 'datadestruction' ? fieldMapping.dataDestructionMiddleName : fieldMapping.consentMiddleName;
+    const middleName =  participant[middleNameCID] ? ` ${participant[middleNameCID]} ` : ' ';
     const lastName = participant[fieldMapping.consentLastName]
     const suffixCID = type === 'hipaarevoc' ? fieldMapping.HIPAARevocSuffix : type === 'datadestruction' ? fieldMapping.dataDestructionSuffix : fieldMapping.consentSuffix;
     const suffix = participant[suffixCID] ? ` ${fieldMapping[participant[suffixCID]]}` : ''
