9fb74168b68b15979c34ce658e65d6fbfbc6a3be
abhinavjonnada82 <abhinav.jonnada@gmail.com>
2024-01-16 20:01:03 -0500

    add auto collapse menu on click

--- siteManagerDashboard/navigationBar.js
+++ siteManagerDashboard/navigationBar.js
@@ -13,45 +13,45 @@ export const dashboardNavBarLinks = (isParent) => {
     const helpDesk = localStorage.getItem('helpDesk');
     return `
         <li class="nav-item">
-            <a class="nav-item nav-link ws-nowrap" href="#home" title="Home" id="dashboardBtn"><i class="fas fa-home"></i> Home</a>
+            <a class="nav-item nav-link ws-nowrap" href="#home" title="Home" id="dashboardBtn"><span data-target="#navbarNavAltMarkup" data-toggle="collapse"><i class="fas fa-home"></i> Home</span></a>
         </li>
         <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle ws-nowrap" id="participants" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-users"></i> Participants
             </a>
             <div class="dropdown-menu sub-div-shadow" aria-labelledby="participants">
-                <a class="dropdown-item" href="#participants/notyetverified" id="notVerifiedBtn">Not Yet Verified Participants</a>
-                <a class="dropdown-item" href="#participants/cannotbeverified" id="cannotVerifiedBtn">Cannot Be Verified Participants</a>
-                <a class="dropdown-item" href="#participants/verified" id="verifiedBtn">Verified Participants</a>
-                <a class="dropdown-item" href="#participants/all" id="allBtn">All Participants</a>
-                <a class="dropdown-item" href="#participants/profilenotsubmitted" id="profileNotSubmitted">Profile Not Submitted</a>
-                <a class="dropdown-item" href="#participants/consentnotsubmitted" id="consentNotSubmitted"> Consent Not Submitted</a>
-                <a class="dropdown-item" href="#participants/notsignedin" id="notSignedIn">Not Signed In</a>
+                <a class="dropdown-item" href="#participants/notyetverified" id="notVerifiedBtn"><span data-target="#navbarNavAltMarkup" data-toggle="collapse">Not Yet Verified Participants</span></a>
+                <a class="dropdown-item" href="#participants/cannotbeverified" id="cannotVerifiedBtn"><span data-target="#navbarNavAltMarkup" data-toggle="collapse">Cannot Be Verified Participants</span></a>
+                <a class="dropdown-item" href="#participants/verified" id="verifiedBtn"><span data-target="#navbarNavAltMarkup" data-toggle="collapse">Verified Participants</span></a>
+                <a class="dropdown-item" href="#participants/all" id="allBtn"><span data-target="#navbarNavAltMarkup" data-toggle="collapse">All Participants</span></a>
+                <a class="dropdown-item" href="#participants/profilenotsubmitted" id="profileNotSubmitted"><span data-target="#navbarNavAltMarkup" data-toggle="collapse">Profile Not Submitted</span></a>
+                <a class="dropdown-item" href="#participants/consentnotsubmitted" id="consentNotSubmitted"><span data-target="#navbarNavAltMarkup" data-toggle="collapse">Consent Not Submitted</span></a>
+                <a class="dropdown-item" href="#participants/notsignedin" id="notSignedIn"><span data-target="#navbarNavAltMarkup" data-toggle="collapse">Not Signed In</span></a>
             </div>
         </li>
         <li class="nav-item" id="participantLookupBtn">
-            <a class="nav-item nav-link ws-nowrap" href="#participantLookup" title="Participant Lookup"><i class="fas fa-search"></i> Participant Lookup</a>
+            <a class="nav-item nav-link ws-nowrap" href="#participantLookup" title="Participant Lookup"><span data-target="#navbarNavAltMarkup" data-toggle="collapse"><i class="fas fa-search"></i> Participant Lookup</span></a>
         </li>
         <li class="nav-item" id="participantDetailsBtn">
-            <a class="nav-item nav-link ws-nowrap" href="#participantDetails" title="Participant Details"><i class="fa fa-info-circle"></i> Participant Details</a>
+            <a class="nav-item nav-link ws-nowrap" href="#participantDetails" title="Participant Details"><span data-target="#navbarNavAltMarkup" data-toggle="collapse"><i class="fa fa-info-circle"></i> Participant Details</span></a>
         </li>
         <li class="nav-item" id="participantSummaryBtn">
-            <a class="nav-item nav-link ws-nowrap" href="#participantSummary" title="Participant Summary"><i class="fa fa-id-badge"></i> Participant Summary</a>
+            <a class="nav-item nav-link ws-nowrap" href="#participantSummary" title="Participant Summary"><span data-target="#navbarNavAltMarkup" data-toggle="collapse"><i class="fa fa-id-badge"></i> Participant Summary</span></a>
         </li>
         ${isParent === 'true' ?
         (`<li class="nav-item" id="participantWithdrawalBtn">
-            <a class="nav-item nav-link ws-nowrap" href="#participantWithdrawal" title="Participant Withdrawal"><i class="fa fa-list-alt"></i> Participant Withdrawal</a>
+            <a class="nav-item nav-link ws-nowrap" href="#participantWithdrawal" title="Participant Withdrawal"><span data-target="#navbarNavAltMarkup" data-toggle="collapse"><i class="fa fa-list-alt"></i> Participant Withdrawal</span></a>
         </li>`) : (``)  }
         <li class="nav-item" id="participantMessageBtn">
-            <a class="nav-item nav-link ws-nowrap" href="#participantMessages" title="Participant Messages"><i class="fa fa-envelope-open"></i> Participant Messages</a>
+            <a class="nav-item nav-link ws-nowrap" href="#participantMessages" title="Participant Messages"><span data-target="#navbarNavAltMarkup" data-toggle="collapse"><i class="fa fa-envelope-open"></i> Participant Messages</span></a>
         </li>
         ${(helpDesk === 'true' || coordinatingCenter === 'true') ?
         (`<li class="nav-item" id="participantVerificationBtn">
-            <a class="nav-item nav-link ws-nowrap" href="#participantVerificationTool" title="Participant Verification Tool"><i class="fa fa-check"></i> Participant Verification Tool</a>
+            <a class="nav-item nav-link ws-nowrap" href="#participantVerificationTool" title="Participant Verification Tool"><span data-target="#navbarNavAltMarkup" data-toggle="collapse"><i class="fa fa-check"></i> Participant Verification Tool</span></a>
         </li>`) : (``) }
         ${(isParent !== 'true' || coordinatingCenter === 'true') ?
         (`<li class="nav-item" id="siteMessageBtn">
-            <a class="nav-item nav-link ws-nowrap" href="#siteMessages" title="Site Messages"><i class="fa fa-comments""></i> Site Messages</a>
+            <a class="nav-item nav-link ws-nowrap" href="#siteMessages" title="Site Messages"><span data-target="#navbarNavAltMarkup" data-toggle="collapse"><i class="fa fa-comments""></i> Site Messages</span></a>
         </li>`) : (``) }
         ${coordinatingCenter === 'true' ?
         (`<li class="nav-item dropdown">
@@ -59,8 +59,8 @@ export const dashboardNavBarLinks = (isParent) => {
                     <i class="fa fa-bell"></i> Notifications 
                 </a>
                 <div class="dropdown-menu sub-div-shadow" aria-labelledby="notifications">
-                    <a class="dropdown-item" href="#notifications/createnotificationschema" id="storeNotificationSchema">Create Notification Schema</a>
-                    <a class="dropdown-item" href="#notifications/retrievenotificationschema" id="retrieveNotificationSchema">Retrieve Notification Schema</a>
+                    <a class="dropdown-item" href="#notifications/createnotificationschema" id="storeNotificationSchema"><span data-target="#navbarNavAltMarkup" data-toggle="collapse">Create Notification Schema</span></a>
+                    <a class="dropdown-item" href="#notifications/retrievenotificationschema" id="retrieveNotificationSchema"><span data-target="#navbarNavAltMarkup" data-toggle="collapse">Retrieve Notification Schema</span></a>
                 </div>
             </li>`) : (``) }
         <li class="nav-item">
